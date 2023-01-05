e5136ef6436bed0efa90e89032ee216336ecb77f
RabicaTahir <119584186+RabicaTahir@users.noreply.github.com>
2023-01-05 16:48:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+CSS.registerProperty({
+    name: "--load",
+    syntax: "<integer>",
+    initialValue:0,
+    inherits: true,
+});
+CSS.registerProperty({
+    name: "--load-1",
+    syntax: "<integer>",
+    initialValue:0,
+    inherits: true,
+});
+CSS.registerProperty({
+    name: "--load-3",
+    syntax: "<integer>",
+    initialValue:0,
+    inherits: true,
+});
+
